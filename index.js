6ed41a6f5e612646768061dd72f10dfb3fcd0fcb
Catarina2024111314 <2024111314@estudantes.ips.pt>
2025-12-03 19:11:39 +0000

    update1

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express'); 
+var mustacheExpress = require('mustache-express');
+var path = require('path');
+var fs = require('fs');
+var app = express();
+const port = 8081;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+//N faço a minima para que é que é isto
+/*// Pedido de GET para a raíz
+app.get('/', (req, res) => {
+ res.send(`Verbo: ${req.method} URL: ${req.url}`);
+});
+
+// Pedido de POST para a raíz
+app.post('/', (req, res) => {
+ res.send(`Verbo: ${req.method} URL: ${req.url} BODY: ${req.body}`);
+});*/
+
+//Listen e link na consola
+app.listen(port, () => {
+    console.log(`Servidor a correr em http://localhost:${port}`);
+});
